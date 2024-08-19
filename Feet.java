a3a42d21b19cbef6e29df6aec8331fad0796aa81
DammyMOre <famuyiwadammya@gmail.com>
2024-08-19 18:09:31 +0100

    Add Feet.java file

---
+++ Feet.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+	public class Feet{
+
+		public static void main(String[] args){
+
+		Scanner input= new Scanner(System.in);
+		
+		System.out.print("enter a number in feets");
+		
+		double feets= input.nextDouble();
+
+				double foot = 0.305;
+
+				double metre =feets*foot;
+
+		System.out.printf("feets is %f", metre);
+
+
+
+		
+		
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+}
