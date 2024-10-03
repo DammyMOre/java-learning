258cec5381b2bc019780e9c870aa2cf20a4c544f
DammyMOre <famuyiwadammya@gmail.com>
2024-10-03 12:24:19 +0100

    phaseGateJava

---
+++ phaseGateJava/ReverseSentence.java
@@ -0,0 +1,19 @@
+/*pseudo code
+prompt the user to write a sentence
+reverse the sentence**/
+
+public class ReverseSentence{
+	public static void main(String[] args){
+	String sentence = (there is a tide in the affairs of men)
+
+
+}
+
+public static void String getReverse(String){
+		
+
+
+}
+
+
+} 
